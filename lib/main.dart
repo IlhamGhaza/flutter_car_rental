272bdf4a6d67e24124a80a45443427220b9de86f
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-10-27 18:33:14 +0700

    feat: adds new pages and widgets for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_car_rental/pages/onboarding/splash_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,46 +9,47 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: TextTheme(
+          titleMedium: GoogleFonts.barlow(
+            fontSize: 18,
+            height: 1.1,
+            color: Colors.black,
+            fontWeight: FontWeight.w600,
+          ),
+          bodyLarge: GoogleFonts.barlow(
+            fontSize: 20,
+            color: Colors.white,
+            height: 1.1,
+          ),
+          bodyMedium: GoogleFonts.barlow(
+            fontSize: 16,
+            height: 1.1,
+            color: Colors.white,
+          ),
+          titleLarge: GoogleFonts.barlow(
+            fontSize: 25,
+            height: 1.1,
+            color: Colors.black,
+          ),
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          onPrimary: const Color(0xFF2C2B34),
+        ),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SplashScreen(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  
-  
-  
-
-  
-  
-  
-  
 
   final String title;
 
@@ -59,50 +62,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      
-      
-      
-      
-      
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    
-    
-    
-    
-    
-    
     return Scaffold(
       appBar: AppBar(
-        
-        
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        
         title: Text(widget.title),
       ),
       body: Center(
-        
-        
         child: Column(
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -119,7 +91,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
